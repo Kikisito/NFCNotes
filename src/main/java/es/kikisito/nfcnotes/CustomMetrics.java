2604ed747de1b508e05a02c788f7b78bf2c721a7
Kikisito <35898087+Kikisito@users.noreply.github.com>
2020-07-18 20:41:46 +0200

    Fixed and improved custom charts for Metrics

--- src/main/java/es/kikisito/nfcnotes/CustomMetrics.java
+++ src/main/java/es/kikisito/nfcnotes/CustomMetrics.java
@@ -5,6 +5,21 @@ import org.bstats.bukkit.Metrics;
 
 public class CustomMetrics {
     public CustomMetrics(Main plugin, Metrics metrics) {
-        metrics.addCustomChart(new Metrics.SimplePie("used_language", NFCConfig.NOTE_MATERIAL::getString));
+        // Material used
+        metrics.addCustomChart(new Metrics.SimplePie("material_used", NFCConfig.NOTE_MATERIAL::getString));
+        // Decimal Format
+        metrics.addCustomChart(new Metrics.SimplePie("decimal_format", NFCConfig.NOTE_DECIMAL_FORMAT::getString));
+        // Does the plugin warn staff?
+        metrics.addCustomChart(new Metrics.SimplePie("warn_staff_enabled", () -> {
+            String warn_staff = "Disabled";
+            if(NFCConfig.MODULES_WARN_STAFF.getBoolean()) warn_staff = "Enabled";
+            return warn_staff;
+        }));
+        // Is Update Checker enabled?
+        metrics.addCustomChart(new Metrics.SimplePie("update_checker_enabled", () -> {
+            String update_checker = "Disabled";
+            if(NFCConfig.MODULES_WARN_STAFF.getBoolean()) update_checker = "Enabled";
+            return update_checker;
+        }));
     }
 }
